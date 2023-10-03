24cd4c822a903149670712ff4836a4e0399c38f7
Alexandra Tinjacá <71520459+Epifis@users.noreply.github.com>
2023-10-03 10:59:38 -0500

    Update TiendaDeProductosConArreglos.java

--- TiendaDeProductosConArreglos.java
+++ TiendaDeProductosConArreglos.java
@@ -23,26 +23,19 @@ public class TiendaDeProductosConArreglos {
         Producto producto;
         producto = new Producto(); 
         
-        /*
-        tienda1.UsarArregloDeFabrica();
-        
-        tienda1.AumentarValorUnitario();
-        tienda1.ProductoMenosVendido();
-        tienda1.ProductoMasVendido();
-        tienda1.RealizarVenta();
-        */
-        
         do{
             System.out.println("********** Tienda1 **********");
             System.out.println("1. Crear una tienda ");
             System.out.println("2. Agregar productos a la tienda ");
             System.out.println("3. Usar los productos de fabrica ");
             System.out.println("4. Mostrar productos de la tienda ");
-            System.out.println("5. Producto más vendido en la tienda");
-            System.out.println("6. Producto menos vendido en la tienda");
-            System.out.println("7. Valor total de ventas de la tienda");
-            System.out.println("8. Valor de IVA a consignar por la tienda");
-            System.out.println("9. Calcular descuento (incluye valor del iva)");
+            System.out.println("5. Producto más vendido en la tienda ");
+            System.out.println("6. Producto menos vendido en la tienda ");
+            System.out.println("7. Valor total de ventas de la tienda ");
+            System.out.println("8. Valor de IVA a consignar por la tienda ");
+            System.out.println("9. Revisar si hay que realizar pedidos de productos ");
+            System.out.println("10. Realizar la compra de un producto ");
+            System.out.println("11. Aumentar el valor unitario según el tipo ");
             System.out.println("");
             System.out.println("0. Terminar");
             System.out.println("Por favor ingrese el número de la opción deseada: ");
@@ -56,13 +49,12 @@ public class TiendaDeProductosConArreglos {
                 case 6 -> tienda1.ProductoMenosVendido();
                 case 7 -> tienda1.MostrarValorTotalDeVenta();
                 case 8 -> tienda1.ValorDeIVA();
+                case 9 -> tienda1.SuficienciaDeCantidad();
+                case 10 -> tienda1.RealizarVenta();
+                case 11 -> tienda1.ModificarSegunPoliticaDeTipo();
                 default -> opcion = 0;
             }
-            
         }while(opcion != 0);
-        
         System.out.println("Sesión terminada.");
-        
     }
-    
 }
