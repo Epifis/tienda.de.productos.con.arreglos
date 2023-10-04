f8745318bdc8ab3ffe7a29b6e1b174fe621f7ea4
Alexandra Tinjacá <71520459+Epifis@users.noreply.github.com>
2023-10-04 17:04:29 -0500

    Update Tienda.java

--- Tienda.java
+++ Tienda.java
@@ -1,13 +1,45 @@
 package tiendadeproductos;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  *
  * @author Kotaro
  */
 public class Tienda {
+    private int maximo=0, actual=0;
+    
     public Tienda(){
+    }
+    public void AgregarProductos(Producto[] productos){
+        Scanner scanner = new Scanner (System.in);
+        if (actual < this.maximo){
+            System.out.println("Ingrese los detalles del producto:");
+
+            System.out.print("Nombre: ");
+            String nombre = scanner.nextLine();
+
+            System.out.print("Tipo: ");
+            String tipo = scanner.nextLine();
+
+            System.out.print("Cantidad vendida: ");
+            int cantidadVendida = Integer.parseInt(scanner.nextLine());
+
+            System.out.print("Cantidad actual: ");
+            int cantidadActual = Integer.parseInt(scanner.nextLine());
+
+            System.out.print("Cantidad mínima: ");
+            int cantidadMinima = Integer.parseInt(scanner.nextLine());
+
+            System.out.print("Precio por unidad: ");
+            double precioPorUnidad = Double.parseDouble(scanner.nextLine());
+            Producto productoNuevo = new Producto(nombre, tipo, cantidadVendida, cantidadActual, cantidadMinima, precioPorUnidad);
+            productos[actual] = productoNuevo;
+            this.actual++;
+        }
+        else
+            System.out.println("No se pueden agregar más productos, haz alcanzado el máximo.");
     }
     public Producto ProductoMenosVendido(Producto[] productos) {
         Producto menosVendido = productos[0];  // Suponemos que el primer producto es el menos vendido inicialmente
@@ -33,12 +65,13 @@ public class Tienda {
     }
     public String verificarTipoProducto(Producto producto) {
             String tipo = producto.getTipo();
-            return switch (tipo) {
+            tipo= switch (tipo) {
                 case "1" -> "Supermercado";
                 case "2" -> "Droguería";
                 case "3" -> "Papelería";
                 default -> "Desconocido";
             };
+        return tipo;
         }
     public void mostrarProductos(Producto[] productos) {
             System.out.println("Los productos almacenados en la tienda son: " + Arrays.deepToString(productos));
@@ -107,4 +140,49 @@ public class Tienda {
         System.out.println("En inventario hay " + cantTotalInventario + "productos en total" + 
                 "\n su valor Total es de $" + valorTotalInventario);
     }
+    public void PrecioPromedioProductos(Producto[] productos){
+        double valorTemporalPrecio = 0, precioPromedio;
+        
+        for (Producto producto : productos) { //Aqui se hizo la prueba con for-loop
+            valorTemporalPrecio += producto.getPrecioPorUnidad();
+        }
+        precioPromedio = valorTemporalPrecio / productos.length;
+        System.out.println("El precio promedio en la tienda es de " + precioPromedio);
+    }
+    //Calcular el dinero en caja para cada tipo de producto
+    public void DineroEnCajaPorTipo(Producto[] productos){
+        double dineroCaja1 = 0, dineroCaja2 = 0, dineroCaja3 = 0;
+        int cantTipo1 = 0, cantTipo2 = 0, cantTipo3 = 0;
+        
+        for (Producto producto : productos) {
+            switch (producto.getTipo()) {
+                case "1" -> {
+                    cantTipo1 += producto.getCantidadVendida();
+                    dineroCaja1 += (producto.getPrecioPorUnidad() * producto.getCantidadVendida());
+                }
+                case "2" -> {
+                    cantTipo2 += producto.getCantidadVendida();
+                    dineroCaja2 += (producto.getPrecioPorUnidad() * producto.getCantidadVendida());
+                }
+                case "3" -> {
+                    cantTipo3 += producto.getCantidadVendida();
+                    dineroCaja3 += (producto.getPrecioPorUnidad() * producto.getCantidadVendida());
+                }
+            }
+        }
+        
+        System.out.println("En tipo 1 hay " + cantTipo1 + " productos, y la cantidad de dinero en caja por tipo es de: " + dineroCaja1);
+        System.out.println("En tipo 2 hay " + cantTipo2 + " productos, y la cantidad de dinero en caja por tipo es de: " + dineroCaja2);
+        System.out.println("En tipo 2 hay " + cantTipo3 + " productos, y la cantidad de dinero en caja por tipo es de: " + dineroCaja3);
+        
+    }
+
+    public void setMaximo(int maximo) {
+        this.maximo = maximo;
+    }
+
+    public void setActual(int actual) {
+        this.actual = actual;
+    }
+    
 }
