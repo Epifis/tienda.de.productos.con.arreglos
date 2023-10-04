7f1a7b3f775b6d09f64425dd36e0032ad4123aff
Alexandra Tinjacá <71520459+Epifis@users.noreply.github.com>
2023-10-03 22:59:29 -0500

    Update Producto.java
    Con getter y setter

--- Producto.java
+++ Producto.java
@@ -1,44 +1,83 @@
 package tienda.de.productos.con.arreglos;
 
-import java.util.Scanner;
-import static tienda.de.productos.con.arreglos.Tienda.ProductoEnTienda;
-
 
 /**
  *
  * @author Alexandra Tinjaca
  */
 public class Producto {
-    public int tipo, cantBodega,cantMinima, cantVendida, precioBase;
-    private static final Scanner sc = new Scanner(System.in);
+    private String nombre;
+    private String tipo;
+    private int cantidadVendida;
+    private int cantidadActual;
+    private int cantidadMinima;
+    private double precioPorUnidad;
 
-    public Producto(){
-    }
-    public void UsarArregloDeFabrica(String [][] productoEnTienda){
-        System.out.println("Por favor ingrese el número del producto");
-        int x = sc.nextInt();
-        switch(x){
-            case 0 -> x=0;
-            case 1 -> x=1;
-            case 2 -> x=2;
-            case 4 -> x=4;
-            default -> x=0;
-        }
-        this.tipo = Integer.parseInt(productoEnTienda[1][x]);
-        this.cantBodega = Integer.parseInt(productoEnTienda[2][x]);
-        this.cantMinima = Integer.parseInt(productoEnTienda[3][x]);
-        this.precioBase = Integer.parseInt(productoEnTienda[4][x]);
-        
-        System.out.println("Precio: " + precioBase);
-        System.out.println("Tipo: " + tipo);
-        System.out.println("Cantidad en Bodega: " + cantBodega);
-        System.out.println("Tope mínimo: " + cantMinima);
+    public Producto(String nombre, String tipo, int cantidadVendida, int cantidadActual, int cantidadMinima, double precioPorUnidad) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.cantidadVendida = cantidadVendida;
+        this.cantidadActual = cantidadActual;
+        this.cantidadMinima = cantidadMinima;
+        this.precioPorUnidad = precioPorUnidad;
     }
 
-    public String[][] getProducto() {
-        return ProductoEnTienda;
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre +
+                "\nTipo: " + tipo +
+                "\nCantidad vendida: " + cantidadVendida +
+                "\nCantidad actual: " + cantidadActual +
+                "\nCantidad mínima: " + cantidadMinima +
+                "\nPrecio por unidad: " + precioPorUnidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getCantidadVendida() {
+        return cantidadVendida;
+    }
+
+    public int getCantidadActual() {
+        return cantidadActual;
+    }
+
+    public int getCantidadMinima() {
+        return cantidadMinima;
+    }
+
+    public double getPrecioPorUnidad() {
+        return precioPorUnidad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setCantidadVendida(int cantidadVendida) {
+        this.cantidadVendida = cantidadVendida;
+    }
+
+    public void setCantidadActual(int cantidadActual) {
+        this.cantidadActual = cantidadActual;
+    }
+
+    public void setCantidadMinima(int cantidadMinima) {
+        this.cantidadMinima = cantidadMinima;
+    }
+
+    public void setPrecioPorUnidad(double precioPorUnidad) {
+        this.precioPorUnidad = precioPorUnidad;
     }
     
-    
-
 }
