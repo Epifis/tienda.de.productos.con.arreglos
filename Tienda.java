df95e7f9f02bf8cb5579fb3b692c5e7932f887be
Alexandra Tinjacá <71520459+Epifis@users.noreply.github.com>
2023-10-04 15:46:48 -0500

    Update Tienda.java

--- Tienda.java
+++ Tienda.java
@@ -1,4 +1,6 @@
-package tienda.de.productos.con.arreglos;
+package tiendadeproductos;
+
+import java.util.Arrays;
 
 /**
  *
@@ -18,7 +20,6 @@ public class Tienda {
 
         return menosVendido;
     }
-
     public Producto ProductoMasVendido(Producto[] productos) {
     Producto masVendido = productos[0];  // Suponemos que el primer producto es el más vendido inicialmente
 
@@ -30,56 +31,44 @@ public class Tienda {
 
     return masVendido;
     }
-
-public String verificarTipoProducto(Producto producto) {
-        String tipo = producto.getTipo();
-        switch (tipo) {
-            case "1":
-                return "Supermercado";
-            case "2":
-                return "Droguería";
-            case "3":
-                return "Papelería";
-            default:
-                return "Desconocido";
+    public String verificarTipoProducto(Producto producto) {
+            String tipo = producto.getTipo();
+            return switch (tipo) {
+                case "1" -> "Supermercado";
+                case "2" -> "Droguería";
+                case "3" -> "Papelería";
+                default -> "Desconocido";
+            };
         }
+    public void mostrarProductos(Producto[] productos) {
+            System.out.println("Los productos almacenados en la tienda son: " + Arrays.deepToString(productos));
     }
-public void calcularValorUnitario(Producto[] productos, int indice) {
-    if (indice >= 0 && indice < productos.length) {
-        Producto producto = productos[indice];
-        double valorUnitario = producto.getPrecioPorUnidad() / producto.getCantidadActual();
-        System.out.println("El valor unitario del producto es: " + valorUnitario);
-    } else {
-        System.out.println("Índice de producto no válido.");
-    }
-}
-public void verificarCantidadMinima(Producto[] productos, int indice) {
-    if (indice >= 0 && indice < productos.length) {
-        Producto producto = productos[indice];
-        if (producto.getCantidadActual() >= producto.getCantidadMinima()) {
-            System.out.println("La cantidad actual es mayor o igual a la cantidad mínima.");
+    public void verificarCantidadMinima(Producto[] productos, int indice) {
+        if (indice >= 0 && indice < productos.length) {
+            Producto producto = productos[indice];
+            if (producto.getCantidadActual() >= producto.getCantidadMinima()) {
+                System.out.println("La cantidad actual es mayor o igual a la cantidad mínima.");
+            } else {
+                System.out.println("La cantidad actual es menor que la cantidad mínima. Se debe hacer un pedido.");
+                HacerPedido(productos, indice);
+            }
         } else {
-            System.out.println("La cantidad actual es menor que la cantidad mínima. Se debe hacer un pedido.");
-            // Lógica para hacer un pedido y actualizar la cantidad actual del producto
+            System.out.println("Índice de producto no válido.");
         }
-    } else {
-        System.out.println("Índice de producto no válido.");
     }
-}
-public static double calcularValorIVA(Producto producto) {
-        double iva;
-        iva = switch (producto.getTipo()) {
-            case "1" -> 0.03;
-            case "2" -> 0.02;
-            case "3" -> 0.13;
-            default -> 0.0;
-        }; // Supermercado
-        // Droguería
-        // Papelería
-        // Valor por defecto si el tipo no es válido
-        return iva;
-    }
-
+    public static double calcularValorIVA(Producto producto) {
+            double iva;
+            iva = switch (producto.getTipo()) {
+                case "1" -> 0.03;
+                case "2" -> 0.02;
+                case "3" -> 0.13;
+                default -> 0.0;
+            }; // Supermercado
+            // Droguería
+            // Papelería
+            // Valor por defecto si el tipo no es válido
+            return iva;
+        }
     public void realizarVenta(Producto[] productos, int indice, int cantidadAComprar) {
         if (indice >= 0 && indice < productos.length) {
             Producto producto = productos[indice];
@@ -91,8 +80,10 @@ public static double calcularValorIVA(Producto producto) {
                 System.out.println("Venta realizada correctamente.");
                 System.out.println("Total de la venta (con IVA): $" + totalVenta);
             } else {
-                System.out.println("La cantidad solicitada supera la cantidad actual en inventario.");
+                System.out.println("La cantidad solicitada supera la cantidad actual en inventario, se reabastecera el inventario,"
+                        + " se le hará la venta del producto y se hará el pedido en bodega.");
                 HacerPedido(productos, indice);
+                producto.setCantidadVendida(producto.getCantidadVendida() + cantidadAComprar);
             }
         } else {
             System.out.println("Índice de producto no válido.");
@@ -102,5 +93,18 @@ public static double calcularValorIVA(Producto producto) {
         Producto producto = productos[indice];
         producto.setCantidadActual(producto.getCantidadMinima());
         System.out.println("Ahora la cantidad actual del inventario es de: " + producto.getCantidadActual());
+    }
+    public void CalcularValorDelInventario(Producto[] productos){
+        int cantTotalInventario = 0, valorTemporalCant;
+        double valorTemporalPrecio, valorTotalInventario = 0;
+        
+         for (int i = 1; i < productos.length; i++) {
+            cantTotalInventario += productos[i].getCantidadActual();
+            valorTemporalCant = productos[i].getCantidadActual();
+            valorTemporalPrecio = productos[i].getPrecioPorUnidad();
+            valorTotalInventario += (valorTemporalCant * valorTemporalPrecio);
+        }
+        System.out.println("En inventario hay " + cantTotalInventario + "productos en total" + 
+                "\n su valor Total es de $" + valorTotalInventario);
     }
 }
